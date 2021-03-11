b745fe7ed97d8f02bc9a52b68ac3e8c84d926690
hoangduys4k5 <75007229+hoangduys4k5@users.noreply.github.com>
2021-03-11 13:16:43 +0700

    mvc

---
+++ mavenproject1/src/main/java/MVC/MenuView.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MVC;
+
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author MyPC
+ */
+public class MenuView {
+
+    public MenuView() {
+    }
+    
+    public int hien_thi_menu(){
+        System.out.println("+++++++++++++++++++++++");
+        System.out.println("1. Nhap du lieu");
+        System.out.println("2. Tim kiem ");
+        System.out.println("3. Sua du lieu");
+        System.out.println("4. Xoa du lieu");
+        System.out.println("5. Thoat chung trinh");
+        System.out.println("+++++++++++++++++++++++");
+        System.out.println("Ban chon chuc nang nao?");
+        Scanner sc = new Scanner(System.in);
+        int kq = Integer.parseInt(sc.nextLine());
+        return kq;
+    }
+}
